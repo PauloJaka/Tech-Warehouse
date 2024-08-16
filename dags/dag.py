42b7c415c6dae4648c1829a6fa7285b00493888a
Paulo Jaka <zpaulogamer@gmail.com>
2024-08-16 18:47:52 -0300

    <Refactor> Dag, to use thwe ingest and scrape operator in loop

--- dags/dag.py
+++ dags/dag.py
@@ -1,10 +1,12 @@
-import sys
-sys.path.append('/opt/airflow/dags/scripts')
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
+import sys
+
+sys.path.append('/opt/airflow/dags/scripts')
+
 from Amazon_Scrappy_Products import Amazon_Scrappy_Products
-from ingestion_data import ingest_data_to_postgres
+from tasks import generic_scrape_task, generic_ingest_task
 
 default_args = {
     'owner': 'airflow',
@@ -13,23 +15,37 @@ default_args = {
     'email': ['your_email@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 1,
+    'retries': 0,
     'retry_delay': timedelta(minutes=5),
 }
 
 dag = DAG(
-    'Scrape_Amazon',
+    'Scrape_Multiple_Sites',
     default_args=default_args,
-    description='Uma DAG para fazer scraping da Amazon',
+    description='Uma DAG para fazer scraping de múltiplos sites e ingestão dos dados',
     schedule_interval='@once',
 )
 
-def run_scrapy_and_ingest():
-    df = Amazon_Scrappy_Products()
-    ingest_data_to_postgres(df, 'raw')
+scraping_functions = {
+    'amazon': Amazon_Scrappy_Products
+    #'mercado': Mercado_Scrappy_Products,
+}
 
-scrape_and_ingest_task = PythonOperator(
-    task_id='Scrape_Amazon_and_Ingest',
-    python_callable=run_scrapy_and_ingest,
-    dag=dag,
-)
+for site, scrape_func in scraping_functions.items():
+    scrape_task = PythonOperator(
+        task_id=f'scrape_{site}',
+        python_callable=generic_scrape_task,
+        op_kwargs={'scrape_func': scrape_func},
+        provide_context=True,
+        dag=dag,
+    )
+
+    ingest_task = PythonOperator(
+        task_id=f'ingest_{site}',
+        python_callable=generic_ingest_task,
+        op_kwargs={'table_name': 'raw'},
+        provide_context=True,
+        dag=dag,
+    )
+
+    scrape_task >> ingest_task
