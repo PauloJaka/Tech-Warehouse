308c6e8750b84e80e693459da00586a170a12385
Paulo Jaka <zpaulogamer@gmail.com>
2024-08-17 12:54:45 -0300

    <Refactor> separation of the engine and ingestion, to better aproveitament latter

--- dags/scripts/ingestion_data.py
+++ dags/scripts/ingestion_data.py
@@ -10,16 +10,21 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def ingest_data_to_postgres(df, table_name):
+def get_database_connection():
     user = os.getenv('DB_USER')
     password = os.getenv('DB_PASSWORD')
     host = os.getenv('DB_HOST')
     port = os.getenv('DB_PORT')
     database = os.getenv('DB_NAME')
- 
+
     connection_string = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
-    engine = create_engine(connection_string)    
+    engine = create_engine(connection_string)
+    return engine
+
+def ingest_data_to_postgres(df, table_name):
     try:
+        engine = get_database_connection()
+
         df.to_sql(table_name, engine, schema='lakehouse', if_exists='append', index=False)
         logger.info(f"Dados inseridos na tabela {table_name} com sucesso.")
     except Exception as e:
