76f39dda074610322416b2d3749c534208fc75b2
Paulo Jaka <zpaulogamer@gmail.com>
2024-08-24 22:41:02 -0300

    <Feat> tasks for each scrappy, for better error loging and tratament, also add Mercado Livre scrappy

--- dags/scripts/tasks.py
+++ dags/scripts/tasks.py
@@ -1,28 +1,33 @@
 from ingestion_data import ingest_data_to_postgres, get_database_connection, get_existing_data, update_existing_data
 from Amazon_Scrappy_Products import Amazon_Scrappy_Products
+from mercado_livre_Scrappy_Products import Mercado_Livre_Scrappy_Products
 
-SCRAPE_FUNCTIONS = {
-    'Amazon': Amazon_Scrappy_Products
-   # 'mercado': Mercado_Scrappy_Products,
-    # Adicione outros sites aqui
-}
-
-def run_scrapy_and_ingest_in_raw():
+def run_amazon_scrapy_and_ingest():
     engine = get_database_connection()
 
-    for site, scrape_func in SCRAPE_FUNCTIONS.items():
-        df = scrape_func()
+    df = Amazon_Scrappy_Products()
+    if df is not None and not df.empty:
+        print(f"DataFrame para Amazon:")
+        
+        table_name = "raw"
+        existing_data = get_existing_data(engine)
+        df = update_existing_data(df, existing_data)
+        ingest_data_to_postgres(df, table_name)
+    else:
+        print(f"Erro: DataFrame para Amazon está vazio ou não foi gerado corretamente.")
+        raise ValueError(f"DataFrame para Amazon é vazio ou None")
+    
+def run_mercado_livre_scrapy_and_ingest():
+    engine = get_database_connection()
 
-        if df is not None and not df.empty:
-            print(f"DataFrame para {site}:")
-            
-            table_name = "raw"
-
-            existing_data = get_existing_data(engine)
-
-            df = update_existing_data(df, existing_data)
-
-            ingest_data_to_postgres(df, table_name)
-        else:
-            print(f"Erro: DataFrame para {site} está vazio ou não foi gerado corretamente.")
-            raise ValueError(f"DataFrame para {site} é vazio ou None")
+    df = Mercado_Livre_Scrappy_Products()
+    if df is not None and not df.empty:
+        print(f"DataFrame para Mercado Livre:")
+        
+        table_name = "raw"
+        existing_data = get_existing_data(engine)
+        df = update_existing_data(df, existing_data)
+        ingest_data_to_postgres(df, table_name)
+    else:
+        print(f"Erro: DataFrame para Mercado Livre está vazio ou não foi gerado corretamente.")
+        raise ValueError(f"DataFrame para Mercado Livre é vazio ou None")
