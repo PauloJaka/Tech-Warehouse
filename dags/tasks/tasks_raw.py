d6c1ab74d5ef1bb7ac615196663893c457555fc8
Paulo Jaka <zpaulogamer@gmail.com>
2024-09-07 21:54:43 -0300

    <Feat> tasks with kaluga, kabum, fastshop and casas bahia

--- dags/tasks/tasks_raw.py
+++ dags/tasks/tasks_raw.py
@@ -3,6 +3,10 @@ from dags.web_scrappers.amazon_Scrappy_Products import Amazon_Scrappy_Products
 from dags.web_scrappers.mercado_livre_Scrappy_Products import Mercado_Livre_Scrappy_Products
 from dags.web_scrappers.magalu_Scrappy_Products import Magalu_Scrappy_Products
 from dags.web_scrappers.americanas_Scrappy_Products import Americanas_Scrappy_Products
+from dags.web_scrappers.casas_bahia_Scrappy_Products import Casas_Bahia_Scrappy_Products
+from dags.web_scrappers.kalunga_Scrappy_Products import Kalunga_Scrappy_Products
+from dags.web_scrappers.fastshop_Scrappy_Products import FastShop_Scrappy_Products
+from dags.web_scrappers.kabum_Scrappy_Products import KaBum_Scrappy_Products
 
 def run_amazon_scrapy_and_ingest():
     engine = get_database_connection()
@@ -63,3 +67,64 @@ def run_americanas_scrapy_and_ingest():
     else:
         print(f"Erro: DataFrame para Americanas está vazio ou não foi gerado corretamente.")
         raise ValueError(f"DataFrame para Americanas é vazio ou None")
+    
+def run_casas_bahia_scrapy_and_ingest():
+    engine = get_database_connection()
+
+    df = Casas_Bahia_Scrappy_Products()
+    if df is not None and not df.empty:
+        print(f"DataFrame para Casas Bahia:")
+        
+        table_name = "raw"
+        existing_data = get_existing_data(engine)
+        df = update_existing_data(df, existing_data)
+        ingest_data_to_postgres(df, table_name)
+    else:
+        print(f"Erro: DataFrame para Casas Bahia está vazio ou não foi gerado corretamente.")
+        raise ValueError(f"DataFrame para Casas Bahia é vazio ou None")
+    
+def run_kalunga_scrapy_and_ingest():
+    engine = get_database_connection()
+
+    df = Kalunga_Scrappy_Products()
+    if df is not None and not df.empty:
+        print(f"DataFrame para Kalunga:")
+        
+        table_name = "raw"
+        existing_data = get_existing_data(engine)
+        df = update_existing_data(df, existing_data)
+        ingest_data_to_postgres(df, table_name)
+    else:
+        print(f"Erro: DataFrame para Kalunga está vazio ou não foi gerado corretamente.")
+        raise ValueError(f"DataFrame para Kalunga é vazio ou None")
+
+def run_fastshop_scrapy_and_ingest():
+    engine = get_database_connection()
+
+    df = FastShop_Scrappy_Products()
+    if df is not None and not df.empty:
+        print(f"DataFrame para fastshop:")
+        
+        table_name = "raw"
+        existing_data = get_existing_data(engine)
+        df = update_existing_data(df, existing_data)
+        ingest_data_to_postgres(df, table_name)
+    else:
+        print(f"Erro: DataFrame para fastshop está vazio ou não foi gerado corretamente.")
+        raise ValueError(f"DataFrame para fastshop é vazio ou None")
+
+def run_kabum_scrapy_and_ingest():
+    engine = get_database_connection()
+
+    df = KaBum_Scrappy_Products()
+    if df is not None and not df.empty:
+        print(f"DataFrame para KaBum:")
+        
+        table_name = "raw"
+        existing_data = get_existing_data(engine)
+        df = update_existing_data(df, existing_data)
+        ingest_data_to_postgres(df, table_name)
+    else:
+        print(f"Erro: DataFrame para KaBum está vazio ou não foi gerado corretamente.")
+        raise ValueError(f"DataFrame para KaBum é vazio ou None")
+
