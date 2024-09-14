fb6d826d7b3fac1942ac0e0e1a75d8a774e36c10
Paulo Jaka <zpaulogamer@gmail.com>
2024-09-14 18:38:57 -0300

    <Chore> att silver star-schema model with npl to extract patterns from title

---
+++ dags/models/silver.sql
@@ -0,0 +1,89 @@
+create table lakehouse.f_silver (
+	id int primary key,
+	created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    ingestion_at TIMESTAMP,
+    website varchar(15),
+    category varchar(20)
+);
+
+create table lakehouse.d_silver_notebooks(
+    id int, 
+    title TEXT,
+    discount_price NUMERIC(7, 2),
+    original_price NUMERIC(7, 2),
+    brand varchar(20),
+    rating NUMERIC(3, 2),
+    link TEXT,
+    free_freight BOOLEAN,
+    model varchar(30),
+    CPU varchar(20),
+    GPU varchar(20),
+    RAM varchar(5),
+    SSD varchar(6)
+);
+
+create table lakehouse.d_silver_smartphone (
+    id int, 
+    title TEXT,
+    discount_price NUMERIC(7, 2),
+    original_price NUMERIC(7, 2),
+    brand varchar(20),
+    rating NUMERIC(3, 2),
+    link TEXT,
+    model varchar(30),
+    dual_sim bool,
+    RAM varchar(5),
+    storage_capacity varchar(6)
+)
+
+create table lakehouse.d_silver_tv (
+    id int, 
+    title TEXT,
+    discount_price NUMERIC(7, 2),
+    original_price NUMERIC(7, 2),
+    brand varchar(20),
+    rating NUMERIC(3, 2),
+    link TEXT,
+    model varchar(30),
+	size varchar(4),
+	resolution varchar(15),
+	technology varchar(15)
+);
+
+create table lakehouse.d_silver_tablets(
+    id int, 
+    title TEXT,
+    discount_price NUMERIC(7, 2),
+    original_price NUMERIC(7, 2),
+    brand varchar(20),
+    rating NUMERIC(3, 2),
+    link TEXT,
+    model varchar(30),
+	RAM varchar(5),
+    storage_capacity varchar(6)
+);
+
+create table lakehouse.d_silver_smartwatch(
+    id int, 
+    title TEXT,
+    discount_price NUMERIC(7, 2),
+    original_price NUMERIC(7, 2),
+    brand varchar(20),
+    rating NUMERIC(3, 2),
+    link TEXT,
+    model varchar(30)
+);
+
+ALTER TABLE lakehouse.d_silver_tv 
+ADD CONSTRAINT fk_silver FOREIGN KEY (id) REFERENCES lakehouse.f_silver(id);
+
+ALTER TABLE lakehouse.d_silver_notebooks
+ADD CONSTRAINT fk_silver FOREIGN KEY (id) REFERENCES lakehouse.f_silver(id);
+
+ALTER TABLE lakehouse.d_silver_smartwatch
+ADD CONSTRAINT fk_silver FOREIGN KEY (id) REFERENCES lakehouse.f_silver(id);
+
+ALTER TABLE lakehouse.d_silver_smartphone
+ADD CONSTRAINT fk_silver FOREIGN KEY (id) REFERENCES lakehouse.f_silver(id);
+
