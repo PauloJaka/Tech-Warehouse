fc131979c39fbb8b270f5bd98e2efe95a64ef0fa
Paulo Jaka <zpaulogamer@gmail.com>
2024-09-14 18:39:13 -0300

    <Chore> tasks bronze for ingestion

---
+++ dags/tasks/tasks_bronze.py
@@ -0,0 +1,17 @@
+from ingestion.ingestion_data import insert_into_dimension, insert_into_fact_bronze, get_database_connection
+
+def process_tables_bronze() -> None:
+    engine = get_database_connection()
+    
+    # Define as tabelas e categorias
+    dimension_tables: dict[str, list[str]] = {
+        "d_bronze_smartphone": ["Smartphone"],
+        "d_bronze_tablets": ["Tablet", "Ipad"],
+        "d_bronze_notebooks": ["Notebook"],
+        "d_bronze_tv": ["TV"],
+        "d_bronze_smartwatch": ["Smartwatch"]
+    } 
+    insert_into_fact_bronze(engine)
+    
+    for table, categories in dimension_tables.items():
+        insert_into_dimension(engine, table, categories)
