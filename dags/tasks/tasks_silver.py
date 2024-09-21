7c69c842de9cccd19cf28348153525161683c8f1
Paulo Jaka <zpaulogamer@gmail.com>
2024-09-20 23:39:37 -0300

    <Refactor> tasks silver better modularization to previne error

--- dags/tasks/tasks_silver.py
+++ dags/tasks/tasks_silver.py
@@ -1,19 +1,21 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from ingestion.ingestion_silver import move_data_bronze_to_silver, load_ner_model, insert_data_into_silver_notebook, get_data_from_dimension, apply_ner_to_title
+from ingestion.ingestion_silver import move_data_bronze_to_silver, insert_data_into_silver_notebook, get_data_from_dimension
+from transformation.transformation_silver_notebook import  apply_ner_to_notebook_title, load_ner_model
 
 def process_table_to_silver():
+    move_data_bronze_to_silver()
+
+def process_table_to_silver_notebooks():
     
     move_data_bronze_to_silver()
-    dimension_table_for_data =  'd_bronze_notebooks'
-    dimension_table_for_insert = 'd_silver_notebooks' 
-    df = get_data_from_dimension(dimension_table_for_data)
+    df = get_data_from_dimension('d_bronze_notebooks')
     print(df)
     model_path = '/opt/airflow/dags/utils/trained_models/modelo_ner_notebooks'  
     nlp = load_ner_model(model_path)
 
     # Passo 4: Aplicar NER aos títulos
-    df = apply_ner_to_title(df, nlp)
+    df = apply_ner_to_notebook_title(df, nlp)
     print(df)
-    insert_data_into_silver_notebook(df, dimension_table_for_insert)
+    insert_data_into_silver_notebook(df)
