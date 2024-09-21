2311cf6b70d714f6061d9e680d77d5f7f703a41a
Paulo Jaka <zpaulogamer@gmail.com>
2024-09-20 23:38:54 -0300

    <Refactor> ingestion refactor, better separation between ingestion and transform

--- dags/ingestion/ingestion_silver.py
+++ dags/ingestion/ingestion_silver.py
@@ -1,12 +1,9 @@
-import sqlalchemy as sa
 from datetime import datetime, timedelta
 from sqlalchemy import text
-import spacy
 import pandas as pd
 from .ingestion_raw_and_bronze import get_database_connection, get_max_id
-from utils.utils import normalize_storage, notebook_normalization
 
-def get_data_from_dimension(dimension_table):
+def get_data_from_dimension(dimension_table: str):
     engine = get_database_connection()
     query = f"""
     SELECT id, title, discount_price, original_price, brand, rating, link, free_freight
@@ -35,71 +32,23 @@ def move_data_bronze_to_silver() -> None:
         conn.execute(query, {"ingestion_at": ingestion_at, "max_id_silver": max_id_silver})
         print(f"Dados movidos da tabela bronze para silver com sucesso! Ingestão em: {ingestion_at}")
 
-def load_ner_model(model_path):
-    return spacy.load(model_path)
 
-
-import pandas as pd
-
-def apply_ner_to_title(df, nlp):
-    def entities_to_dataframe(text, doc):
-        entities = {
-            'model': '',
-            'CPU': '',
-            'GPU': '',
-            'RAM': '',
-            'SSD': ''
-        }
-        for ent in doc.ents:
-            if ent.label_ == 'MODEL':
-                entities['model'] = ent.text
-            elif ent.label_ == 'CPU':
-                entities['CPU'] = ent.text
-            elif ent.label_ == 'GPU':
-                entities['GPU'] = ent.text
-            elif ent.label_ == 'RAM':
-                entities['RAM'] = ent.text
-            elif ent.label_ == 'SSD':
-                entities['SSD'] = ent.text
-        return entities
-    
-    def remove_invalid_ssd(df):
-        return df[df['SSD'].apply(lambda x: len(str(x)) <= 6)]
-    def remove_invalid_ram(df):
-        return df[df['RAM'].apply(lambda x: len(str(x)) <= 5)]
-    
-    new_entities = []
-    for title in df['title']:
-        doc = nlp(title)
-        entities = entities_to_dataframe(title, doc)
-        new_entities.append(entities)
-
-    df_entities = pd.DataFrame(new_entities)
-    df = pd.concat([df, df_entities], axis=1)
-    df['RAM'] = df['RAM'].apply(normalize_storage)
-    df['SSD'] = df['SSD'].apply(normalize_storage)
-    
-    df = remove_invalid_ssd(df)
-    df = remove_invalid_ram(df)
-    df = df[df.apply(notebook_normalization, axis=1) != 3]
-    
-    return df
-
-
-def insert_data_into_silver_notebook(df, silver_table):
+def insert_data_into_silver_notebook(df):
     engine = get_database_connection()
-    max_id = get_max_id(engine, silver_table)
+    silver_notebook = 'd_silver_notebooks'
+    max_id = get_max_id(engine, silver_notebook)
+
     
     df_filtered = df[df['id'] > max_id]
 
     if df_filtered.empty:
-        print(f"Nenhum dado novo para inserir na tabela {silver_table}.")
+        print(f"Nenhum dado novo para inserir na tabela {silver_notebook}.")
         return
     
     with engine.connect() as conn:
         for _, row in df_filtered.iterrows():
             query = text(f"""
-            INSERT INTO lakehouse.{silver_table} (
+            INSERT INTO lakehouse.{silver_notebook} (
                 id, title, discount_price, original_price, brand, rating, link, free_freight,
                 model, CPU, GPU, RAM, SSD
             ) VALUES (
@@ -125,4 +74,4 @@ def insert_data_into_silver_notebook(df, silver_table):
                 'SSD': row['SSD']
             }
             conn.execute(query, params)
-    print(f"Dados novos inseridos na tabela {silver_table} com sucesso.")
+    print(f"Dados novos inseridos na tabela {silver_notebook} com sucesso.")
