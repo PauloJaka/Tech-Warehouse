cd48a142c11a8fb3805584801ff89a53b83414ba
Paulo Jaka <zpaulogamer@gmail.com>
2024-09-22 15:41:48 -0300

    <Chore> Smartwatch npl model transformation

---
+++ dags/transformation/silver/smartwatch.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+def apply_ner_to_smartwatch_title(df, nlp) -> pd.DataFrame:
+    def entities_to_dataframe(text, doc):
+        entities = {
+            'model': ''
+        }
+        for ent in doc.ents:
+            if ent.label_ == 'MODEL':
+                entities['model'] = ent.text
+        return entities
+    
+    new_entities = []
+    for title in df['title']:
+        doc = nlp(title)
+        entities = entities_to_dataframe(title, doc)
+        new_entities.append(entities)
+
+    df_entities = pd.DataFrame(new_entities)
+    df = pd.concat([df, df_entities], axis=1)
+    print(len(df))
+    return df
