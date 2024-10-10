ccbd0f4efedbbbca8366ff7ecfc1fe16a828a066
Paulo Jaka <zpaulogamer@gmail.com>
2024-10-10 10:45:58 -0300

    <Chore> ingestion notebook gold layer

--- dags/ingestion/ingestion_gold.py
+++ dags/ingestion/ingestion_gold.py
@@ -21,7 +21,7 @@ def move_data_silver_to_gold() -> None:
         conn.execute(query, {"ingestion_at": ingestion_at, "max_id_gold": max_id_gold})
         print(f"Dados movidos da tabela silver para gold com sucesso! Ingestão em: {ingestion_at}")
 
-def get_new_data_for_gold(silver_table_data: str, insert_table:str) -> pd.DataFrame | None:
+def get_new_data_for_gold(silver_table_data: str, insert_table:str) -> pd.DataFrame:
     engine = get_database_connection()
     max_id_insert = get_max_id(engine, insert_table)
     
@@ -37,3 +37,43 @@ def get_new_data_for_gold(silver_table_data: str, insert_table:str) -> pd.DataFr
         if df.empty:
             raise AirflowSkipException(f"Nenhum dado novo para inserir na tabela {insert_table}.")
     return df
+
+def insert_data_into_gold_notebook(df: pd.DataFrame) -> None:
+    engine = get_database_connection()
+    gold_notebook = 'd_gold_notebooks'
+    
+    if df.empty:
+        raise AirflowSkipException(f"Nenhum dado novo para inserir na tabela {gold_notebook}.")
+    
+    with engine.connect() as conn:
+        for _, row in df.iterrows():
+            query = text(f"""
+            INSERT INTO lakehouse.{gold_notebook} (
+                id, title, discount_price, original_price, brand, rating, link, free_freight,
+                model, CPU, GPU, RAM, SSD, specifics, cpu_category
+            ) VALUES (
+                :id, :title, :discount_price, :original_price, :brand, :rating, :link, :free_freight,
+                :model, :CPU, :GPU, :RAM, :SSD, :specifics, :cpu_category
+            )
+            ON CONFLICT (id) DO NOTHING
+            """)
+
+            params = {
+                'id': row['id'],
+                'title': row['title'],
+                'discount_price': row['discount_price'],
+                'original_price': row['original_price'],
+                'brand': row['brand'],
+                'rating': row['rating'],
+                'link': row['link'],
+                'free_freight': row['free_freight'],
+                'model': row['model'],
+                'CPU': row['CPU'],
+                'GPU': row['GPU'],
+                'RAM': row['RAM'],
+                'SSD': row['SSD'],
+                'specifics': row['specifics'],
+                'cpu_category': row['cpu_category']
+            }
+            conn.execute(query, params)
+    print(f"Dados novos inseridos na tabela {gold_notebook} com sucesso.")
