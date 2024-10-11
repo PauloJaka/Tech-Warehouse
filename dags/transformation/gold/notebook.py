31a564c4d1515f6356d0c85dfb67fb41f13252c5
Paulo Jaka <zpaulogamer@gmail.com>
2024-10-11 18:12:01 -0300

    <Fix> notebook transformation fix, better recall on specifics casual and gamer (high intel ou ryzen)

--- dags/transformation/gold/notebook.py
+++ dags/transformation/gold/notebook.py
@@ -6,38 +6,30 @@ def classify_product_with_regex(row):
     
     intel_high_regex = r'\b(i[579]|core\s*i[579])[\s-]?\d+'
     ryzen_high_regex = r'\b(ryzen\s*[579]|r[579])[\s-]?\d*'
-    intel_low_regex = r'\b(i3|core\s*i3)[\s-]?\d+'
-    ryzen_low_regex = r'\b(ryzen\s*3|r3)[\s-]?\d*'
-    celeron_regex = r'\b(celeron|pentium|atom)'
-    other_basic_regex = r'\b(dual\s*core|quad\s*core|amd\s*a[4-6]|amd\s*athlon)'
+    cpu_str = str(row['CPU']).lower()
+
+    def extract_ram_value(ram_value_str):
+        ram_match = re.search(r'(\d+)', ram_value_str)
+        return int(ram_match.group(1)) if ram_match else None
+
+    ram_value = extract_ram_value(str(row['RAM']))
+    ssd_value_str = str(row['SSD'])
+    ssd_match = re.search(r'(\d+)', ssd_value_str)
 
     if pd.notna(row['GPU']):
         return 'Gamer'
     
-    cpu_str = str(row['CPU']).lower()
-
     if re.search(intel_high_regex, cpu_str) or re.search(ryzen_high_regex, cpu_str):
-        ram_value_str = str(row['RAM'])  
-        ssd_value_str = str(row['SSD'])  
+        if ram_value and ssd_match:
+            ssd_value = int(ssd_match.group(1))
 
-        ram_match = re.search(r'(\d+)', ram_value_str)
-        ssd_match = re.search(r'(\d+)', ssd_value_str)
-
-        if ram_match and ssd_match:
-            ram_value = int(ram_match.group(1)) 
-            ssd_value = int(ssd_match.group(1)) 
-
-            if ram_value > 8 and ssd_value >= 256:
+            if ram_value >= 8 and ssd_value >= 256:
                 return 'Gamer'
 
-    if discount_price < 3000 and '8GB' in str(row['RAM']):
-        if (re.search(intel_low_regex, cpu_str) or 
-            re.search(ryzen_low_regex, cpu_str) or 
-            re.search(celeron_regex, cpu_str) or 
-            re.search(other_basic_regex, cpu_str)):
-            return 'Casual'
+    if discount_price < 2500 and ram_value >= 8:
+        return 'Casual'
     
-    if 'Macbook' in str(row['titulo']):
+    if 'Macbook' in str(row['title']):
         return 'Macbook'
     
     return 'Outros'
@@ -45,20 +37,26 @@ def classify_product_with_regex(row):
 def categorize_cpu(row):
     cpu_str = str(row['cpu']).lower()
     intel_regex = r'\b(i[3579]|core\s*i[3579])[\s-]?\d+'
-    ryzen_regex = r'\b(ryzen\s*[3579]|r[3579])[\s-]?\d*'
+    ryzen_regex = r'\b(ryzen\s*([3579])|r([3579]))[\s-]?\d*'
     celeron_regex = r'\b(celeron|pentium|atom)'
     other_basic_regex = r'\b(dual\s*core|quad\s*core|amd\s*a[4-6]|amd\s*athlon)'
 
-    if re.search(intel_regex, cpu_str):
-        return 'Intel (i3/i5/i7/i9)'
-    elif re.search(ryzen_regex, cpu_str):
-        return 'Ryzen (R3/R5/R7/R9)'
-    elif re.search(celeron_regex, cpu_str):
+    intel_match = re.search(intel_regex, cpu_str)
+    if intel_match:
+        intel_model = intel_match.group(1)  
+        return f'Intel {intel_model.upper()}'
+    
+    ryzen_match = re.search(ryzen_regex, cpu_str)
+    if ryzen_match:
+        ryzen_model = ryzen_match.group(2) or ryzen_match.group(3) 
+        return f'Ryzen R{ryzen_model}'
+    
+    if re.search(celeron_regex, cpu_str):
         return 'Celeron/Pentium/Atom'
-    elif re.search(other_basic_regex, cpu_str):
+    
+    if re.search(other_basic_regex, cpu_str):
         return 'Basic CPU (Dual/Quad Core, Athlon, A4-A6)'
-    else:
-        return 'Unknown CPU'
+    return 'Unknown CPU'
 
 def apply_specifics_categorys_on_notebook(df: pd.DataFrame) -> pd.DataFrame:
     if df is not None:
