48c7f57c71d196e1cdeeeea0401d3aa0783d348d
Paulo Jaka <zpaulogamer@gmail.com>
2024-10-11 17:41:15 -0300

    <Chore> notebook aditional query

---
+++ dags/utils/aditional_querys_gold/notebook.py
@@ -0,0 +1,22 @@
+notebook_query = """ 
+(title ILIKE '%Notebook%' OR title ILIKE '%Macbook%' OR title ILIKE '%Laptop%' OR title ILIKE '%Samsugbook%')
+  AND title NOT ILIKE '%Cabo%'
+  AND title NOT ILIKE '%Mochila%'
+  AND title NOT ILIKE '%Capa%'
+  AND title NOT ILIKE '%Carregador%'
+  AND title NOT ILIKE '%Estojo%'
+ and title not ilike '%Suporte%'
+ and title not ilike '%Base%'
+ and title not ilike '%Fonte%'
+ and title not ilike '%Bateria%'
+ and title not ilike '%Filtro%'
+and title not ilike '%Pasta%'
+and title not ilike '%Trava%'
+and title not ilike '%The Moe Norman Notebook%'
+and title not ilike '%Maleta%'
+AND title NOT ILIKE '%Pelicula%'
+AND title NOT ILIKE '%Película%'
+AND title NOT ILIKE '%Pen drive%'
+and link not ilike '%No link%'
+AND title NOT ILIKE '%Arshray Kit%'; 
+"""
