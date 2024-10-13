c16fafcc77ef30424a399d37fa03e8588417ffd6
Paulo Jaka <zpaulogamer@gmail.com>
2024-10-13 16:01:10 -0300

    <Refactor> smartphone transformation corretion

--- dags/transformation/gold/smartphone.py
+++ dags/transformation/gold/smartphone.py
@@ -7,17 +7,21 @@ def classify_product_with_regex(row):
     if 'iphone' in row['title'].lower():
         return 'iPhone'
     
-    ram_value_str = str(row['RAM'])
+    ram_value_str = str(row['ram'])
     ram_match = re.search(r'(\d+)', ram_value_str)
-    ram_value = int(ram_match.group(1))
+    
+    if ram_match:
+        ram_value = int(ram_match.group(1)) 
+    else:
+        ram_value = None  
 
-    if ram_value:
+    if ram_value is not None:
         if ram_value >= 6:
             return 'Gamer'
-    
-    if discount_price <= 1800 and ram_value >= 4:
-        return 'Casual'
-    
+        
+        if discount_price <= 1800 and ram_value >= 4:
+            return 'Casual'
+        
     if discount_price <= 1200:
         return 'Reserva'
     
@@ -27,4 +31,3 @@ def apply_specifics_categorys_on_smartphone(df: pd.DataFrame) -> pd.DataFrame:
     if df is not None:
         df['specifics'] = df.apply(classify_product_with_regex, axis=1)
     return df
-
