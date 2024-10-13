2d3050761f70e2fb07b40d01975fd5c409c04fb3
Paulo Jaka <zpaulogamer@gmail.com>
2024-10-12 23:43:20 -0300

    <Chore> smartphone transformation

--- dags/transformation/gold/smartphone.py
+++ dags/transformation/gold/smartphone.py
@@ -18,4 +18,13 @@ def classify_product_with_regex(row):
     if discount_price <= 1800 and ram_value >= 4:
         return 'Casual'
     
+    if discount_price <= 1200:
+        return 'Reserva'
+    
     return 'Outros'
+
+def apply_specifics_categorys_on_smartphone(df: pd.DataFrame) -> pd.DataFrame:
+    if df is not None:
+        df['specifics'] = df.apply(classify_product_with_regex, axis=1)
+    return df
+
