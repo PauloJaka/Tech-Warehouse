f7b51c9f9d7bd3a8dc50f35fbb280bc3ebea7c19
Paulo Jaka <zpaulogamer@gmail.com>
2024-10-12 23:42:18 -0300

    <Chore> tablet transformation

---
+++ dags/transformation/gold/tablet.py
@@ -0,0 +1,30 @@
+import re
+import pandas as pd
+
+def classify_product_with_regex(row):
+    discount_price = row['discount_price'] if pd.notna(row['discount_price']) else row['original_price']
+    
+    if 'ipad' in row['title'].lower():
+        return 'Ipad'
+    
+    ram_value_str = str(row['RAM'])
+    ram_match = re.search(r'(\d+)', ram_value_str)
+    ram_value = int(ram_match.group(1))
+
+    if ram_value:
+        if ram_value >= 6:
+            return 'Gamer'
+    
+    if discount_price <= 1999 and ram_value >= 4:
+        return 'Casual'
+    
+    if 'infantil' in row['title'].lower():
+        return 'infantil'
+    
+    return 'Outros'
+
+def apply_specifics_categorys_on_tablet(df: pd.DataFrame) -> pd.DataFrame:
+    if df is not None:
+        df['specifics'] = df.apply(classify_product_with_regex, axis=1)
+    return df
+
