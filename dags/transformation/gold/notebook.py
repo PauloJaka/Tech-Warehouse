02160b103066a4fcfc2249ffdbc95bf0af9d7f72
Paulo Jaka <zpaulogamer@gmail.com>
2024-10-13 16:01:27 -0300

    <Refactor> notebook transformation and agrupation of cpu

--- dags/transformation/gold/notebook.py
+++ dags/transformation/gold/notebook.py
@@ -1,66 +1,71 @@
-import pandas as pd
 import re
+import pandas as pd
+
+def extract_ram_value(ram_value_str):
+    ram_match = re.search(r'(\d+)', ram_value_str)
+    return int(ram_match.group(1)) if ram_match else None
 
 def classify_product_with_regex(row):
-    discount_price = row['discount_price'] if pd.notna(row['discount_price']) else row['original_price']
+    discount_price = row.get('discount_price', row.get('original_price'))
     
     intel_high_regex = r'\b(i[579]|core\s*i[579])[\s-]?\d+'
     ryzen_high_regex = r'\b(ryzen\s*[579]|r[579])[\s-]?\d*'
-    cpu_str = str(row['CPU']).lower()
-
-    def extract_ram_value(ram_value_str):
-        ram_match = re.search(r'(\d+)', ram_value_str)
-        return int(ram_match.group(1)) if ram_match else None
-
-    ram_value = extract_ram_value(str(row['RAM']))
-    ssd_value_str = str(row['SSD'])
-    ssd_match = re.search(r'(\d+)', ssd_value_str)
-
-    if pd.notna(row['GPU']):
-        return 'Gamer'
     
-    if re.search(intel_high_regex, cpu_str) or re.search(ryzen_high_regex, cpu_str):
-        if ram_value and ssd_match:
-            ssd_value = int(ssd_match.group(1))
+    cpu_str = str(row.get('cpu', '')).lower() if pd.notna(row.get('cpu')) else None
 
+    ram_value = extract_ram_value(str(row.get('ram', '')))
+    ssd_value_str = str(row.get('ssd', ''))
+    ssd_value = int(re.search(r'(\d+)', ssd_value_str).group(1)) if re.search(r'(\d+)', ssd_value_str) else None
+
+    if pd.notna(row.get('gpu')) and str(row.get('gpu', '')).strip() != '':
+        return 'Gamer'
+
+    if cpu_str and (re.search(intel_high_regex, cpu_str) or re.search(ryzen_high_regex, cpu_str)):
+        if ram_value is not None and ssd_value is not None:
             if ram_value >= 8 and ssd_value >= 256:
-                return 'Gamer'
-
-    if discount_price < 2500 and ram_value >= 8:
+                return 'Gamer-Without GPU'
+    
+    if ram_value is not None and discount_price is not None and discount_price < 2500 and ram_value >= 8:
         return 'Casual'
     
-    if 'Macbook' in str(row['title']):
+    if 'macbook' in str(row.get('title', '')).lower():
         return 'Macbook'
     
     return 'Outros'
 
 def categorize_cpu(row):
-    cpu_str = str(row['cpu']).lower()
-    intel_regex = r'\b(i[3579]|core\s*i[3579])[\s-]?\d+'
-    ryzen_regex = r'\b(ryzen\s*([3579])|r([3579]))[\s-]?\d*'
+    cpu_str = str(row.get('cpu', '')).lower() if pd.notna(row.get('cpu')) else None
+    
+    if not cpu_str:
+        return None
+
+    intel_regex = r'\b(core\s*i[3579]|i[3579])\s*\d*'
+    ryzen_regex = r'\b(ryzen\s*([3579])|r([3579]))\s*\d*'
     celeron_regex = r'\b(celeron|pentium|atom)'
     other_basic_regex = r'\b(dual\s*core|quad\s*core|amd\s*a[4-6]|amd\s*athlon)'
 
-    intel_match = re.search(intel_regex, cpu_str)
-    if intel_match:
-        intel_model = intel_match.group(1)  
-        return f'Intel {intel_model.upper()}'
+    if re.search(intel_regex, cpu_str):
+        intel_matches = re.findall(intel_regex, cpu_str)
+        intel_models = [match.strip().capitalize() for match in intel_matches]
+        return ', '.join(set(intel_models))  
     
-    ryzen_match = re.search(ryzen_regex, cpu_str)
-    if ryzen_match:
-        ryzen_model = ryzen_match.group(2) or ryzen_match.group(3) 
-        return f'Ryzen R{ryzen_model}'
+    if re.search(ryzen_regex, cpu_str):
+        ryzen_model = re.search(ryzen_regex, cpu_str)
+        return f'Ryzen R{ryzen_model.group(2) or ryzen_model.group(3)}'  # Retorna o modelo correto
     
     if re.search(celeron_regex, cpu_str):
         return 'Celeron/Pentium/Atom'
     
     if re.search(other_basic_regex, cpu_str):
         return 'Basic CPU (Dual/Quad Core, Athlon, A4-A6)'
+    
     return 'Unknown CPU'
 
+
+
 def apply_specifics_categorys_on_notebook(df: pd.DataFrame) -> pd.DataFrame:
-    if df is not None:
+    """Aplica as classificações e categorização às colunas do DataFrame."""
+    if df is not None and not df.empty:
         df['specifics'] = df.apply(classify_product_with_regex, axis=1)
         df['cpu_category'] = df.apply(categorize_cpu, axis=1)
-    
     return df
