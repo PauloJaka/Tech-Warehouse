672da7fc53bbcf45c415a84d9d04fae91fbb4c36
Paulo Jaka <zpaulogamer@gmail.com>
2024-10-17 23:34:14 -0300

    <Chore> more resumes tables on gold_resumes, one for linear regression and the other we gonna use with LLM models to get reviews of the products(and images)

--- dags/models/gold_resumes.sql
+++ dags/models/gold_resumes.sql
@@ -73,3 +73,94 @@ FROM (
 ) AS all_titles
 GROUP BY produto, website, brand
 order by website;
+
+WITH ranked_products AS (
+    SELECT 
+        produto,
+        title AS num_produtos,
+        website,
+        brand,
+        discount_price,
+        created_at,
+        updated_at,
+        link,
+        ROW_NUMBER() OVER (PARTITION BY produto, website, brand ORDER BY updated_at ASC) AS rn 
+    FROM (
+        SELECT 'notebooks' AS produto, dgn.title, dgn.discount_price, fg.website, dgn.brand, fg.created_at, fg.updated_at,dgn.link
+        FROM d_gold_notebooks dgn
+        JOIN f_gold fg ON dgn.id = fg.id
+        UNION ALL
+        SELECT 'smartphones' AS produto, dgs.title, dgs.discount_price, fg.website, dgs.brand, fg.created_at, fg.updated_at,dgs.link
+        FROM d_gold_smartphone dgs
+        JOIN f_gold fg ON dgs.id = fg.id
+        UNION ALL
+        SELECT 'tv' AS produto, dgtv.title, dgtv.discount_price, fg.website, dgtv.brand, fg.created_at, fg.updated_at,dgtv.link
+        FROM d_gold_tv dgtv
+        JOIN f_gold fg ON dgtv.id = fg.id
+        UNION ALL
+        SELECT 'tablets' AS produto, dgt.title, dgt.discount_price, fg.website, dgt.brand, fg.created_at, fg.updated_at,dgt.link
+        FROM d_gold_tablets dgt
+        JOIN f_gold fg ON dgt.id = fg.id
+        UNION ALL
+        SELECT 'smartwatchs' AS produto, dgs2.title, dgs2.discount_price, fg.website, dgs2.brand, fg.created_at, fg.updated_at,dgs2.link
+        FROM d_gold_smartwatch dgs2
+        JOIN f_gold fg ON dgs2.id = fg.id
+    ) AS all_titles
+)
+SELECT 
+    produto,
+    num_produtos,
+    website,
+    brand,
+    created_at,
+    updated_at,
+    discount_price,
+    link
+FROM ranked_products
+ORDER BY produto, website, brand, created_at, updated_at ASC;
+
+
+WITH latest_notebooks AS (
+    SELECT 
+        dgn.title,
+        dgn.discount_price,
+        dgn.original_price,
+        dgn.brand,
+        dgn.rating,
+        dgn.link,
+        dgn.free_freight,
+        dgn.model,
+        dgn.cpu,
+        dgn.gpu,
+        dgn.ram,
+        dgn.ssd,
+        dgn.specifics,
+        dgn.cpu_category,
+        fg.created_at,
+        fg.updated_at,
+        fg.website,
+        ROW_NUMBER() OVER (PARTITION BY dgn.title, fg.created_at ORDER BY fg.updated_at DESC) AS rn
+    FROM d_gold_notebooks dgn
+    JOIN f_gold fg ON dgn.id = fg.id
+)
+SELECT 
+    title, 
+    discount_price, 
+    original_price, 
+    brand, 
+    rating, 
+    link, 
+    free_freight, 
+    model, 
+    cpu, 
+    gpu, 
+    ram, 
+    ssd, 
+    specifics, 
+    cpu_category, 
+    created_at, 
+    updated_at, 
+    website
+FROM latest_notebooks
+WHERE rn = 1 
+ORDER BY created_at, title;
